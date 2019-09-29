59145dabe1b01344cb90835145f2f1ce574c0c13
Michael Weitman <commerce@emwhite.org>
2019-09-29 18:31:10 -0400

    rewritten, needs styling

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Angry Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Smashburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('McRib', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('White Castle', true);
